cbd8f40c3f039666256383dda5e0ce5d2c65f345
keplian <keplian@gmail.com>
2021-12-03 12:28:47 +0300

    all test are writed

---
+++ task_3/1_2.py
@@ -0,0 +1,17 @@
+import author
+
+print(author.long_heavy(1))
+# Время выполнения функции: 1.0 секунд
+# 2
+print(author.long_heavy(1))
+# Время выполнения функции: 0.0 секунд
+# 2
+print(author.long_heavy(2))
+# Время выполнения функции: 1.0 секунд
+# 4
+print(author.long_heavy(2))
+# Время выполнения функции: 0.0 секунд
+# 4
+print(author.long_heavy(2))
+# Время выполнения функции: 0.0 секунд
+# 4
--- task_3/test.py
+++ task_3/test.py
@@ -1,7 +1,26 @@
 import pytest
-import usercode
+import author
+import time
 
 class TestDecorator:
+
     def test_once_call(self):
-        input = 10
-        assert usercode(input) =
+        assert author.long_heavy(100) == 200
+
+    def test_time(self):
+        start = time.time()
+        author.long_heavy(555)
+        stop = time.time()
+        assert round(stop - start) == 1, 'Время выполнения не равно 1 секунде при первом вызове функции long_heavy'
+
+    def test_few_calls(self):
+        start = time.time()
+        author.long_heavy(800)
+        stop = time.time()
+        assert round(
+            stop - start) == 1, 'Время выполнения не равно 1 секунде при первом вызове функции long_heavy'
+        start1 = time.time()
+        author.long_heavy(800)
+        stop1 = time.time()
+        assert round(
+            stop1 - start1) == 0, 'Время выполнения не равно 0 секунд при повторSном вызове функции long_heavy с тем же аргументом'
